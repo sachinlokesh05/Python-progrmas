163ac17710af78ffb2cc289ab64005883ac31ce3
sachinlokesh05 <56384957+sachinlokesh05@users.noreply.github.com>
2019-11-03 12:51:49 +0530

    In progress

--- Calender.py
+++ Calender.py
@@ -1,128 +1,103 @@
-class Calendar:
+from Linkedlist_Utility.Linkedlist_Utility import Singlelinkedlist
 
- #See if given year is a leap year or not
-    def leapYear(self, year):
-        if len(year) == 4:
-            if int(year) % 400 == 0:
-                if int(year) % 100 == 0:
-                    if int(year) % 4 == 0:
+sl=Singlelinkedlist()
+
+class Calender:
+    #see if the year is leapyear or not
+    def leapyear(self,year):
+        if len(year)==4:
+            if int(year)%400==0:
+                if int(year)%100==0:
+                    if int(year)%4==0:
                         return True
                     else:
-                        return False
+                        return  False
                 else:
-                     return True
+                    return True
             else:
-                  return False
+                return False
         else:
-            return True
+            print("invalid year")
 
- # Find thw number of days in given month
-    def month_day(self,month,year):
-        if month == 'January' or 'March' or 'May' or 'July' or 'August' \
-                or 'October' or 'December':
-            number_of_days = 31
-#         elif month == "April" or 'June' or 'September' or 'November' :
-            number_of_days = 30
-        elif month == 'February':
-            if obj.leapYear(year) == True:
-                number_of_days = 29
+    #see the number of days in the given month
+    def month_days(self,month,year):
+        if month=="January" or "March" or " May" or "July" or "August" or "Octomber" or  "December":
+            Number_of_days=31
+        elif month=="April" or "June" or " September" or "November":
+            Number_of_days=30
+        elif month=="February":
+            if obj.leapyear(year):
+                Number_of_days=29
             else:
-                number_of_days = 28
-        return  number_of_days
+                Number_of_days=28
+        return  Number_of_days
 
+    #Find the which in the 1st of tha month
+    def Week_day(self,month,year):
+        year1=year-((14-month)//12)
+        x=year1+(year1//400)+(year1//4)-(year1//100)
+        month1=month+12*((14-month)//12)-2
+        day1=(1+x+(31*month1)//12)%7
+        return day1
 
-# # Find wich day it is on 1sst of that month
-    def week_day(self, month, year):
-
-        year1 = year - ((14 - month) / 12)
-        x = year1 + (year1/4) - (year1/100) + (year1/400)
-        month1 = month + 12 * ((14 - month) / 12) - 2
-        day1 = (1 + x + (31*month1) / 12) % 7
-        return int(day1)
-
-
-#  # Convert month into coreesponding integer value for calculation
+    #convert the month into corresponding interger value
     def month_int(self,month):
-        if month == 'January':
+        if month=="January":
             return 1
-        if month == 'February':
+        elif month=="February":
             return 2
-        if month == 'March':
+        elif month=="March":
             return 3
-        if month == 'April':
+        elif month=="April":
             return 4
-        if month == 'May':
+        elif month=="May":
             return 5
-        if month == 'June':
+        elif month=="June":
             return 6
-        if month == 'July':
+        elif month=="July":
             return 7
-        if month == 'August':
+        elif month=="August":
             return 8
-        if month == 'September':
+        elif month=="September":
             return 9
-        if month == 'October':
+        elif month=="Octomber":
             return 10
-        if month == 'November':
+        elif month=="November":
             return 11
-        if month == 'December':
+        elif month=="December":
             return 12
 
+obj=Calender()
+week=["mon","tue","wed","thurs","fri","sat","sun"]
+try:
+    month=input("enter the month : ")
+    year=int(input("enter the year : "))
+except:
+    print("enter the valid month and year")
+    print("ex: 2019 April")
 
-week = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']
-obj = Calendar()
-year = int(input("Enter year:"))
-month = input("Enter month : ")
-number_of_days = obj.month_day(month, year)
-print(str(month) + " has " + str(number_of_days) + " number of days" + "coz year is "
-      + str(year))
-month_i = obj.month_int(month)
-day = obj.week_day(month_i, year)
-print("Day is : ", week[day])
+number_of_days=obj.month_days(month,year)
+print("number of days ",str(number_of_days),"in the month of ",month,"bcoz the year is ",str(year))
+month_i=obj.month_int(month)
+day=obj.Week_day(month_i,year)
+print("day is",week[day])
 
-calender_row = []
-print('Mon', 'Tue', 'Wed','Thur', 'Fri','Sat','Sun')
+Calender_row=[]
 
-for month_day in range(1, number_of_days):
-    # for i in range(day):
-        # calender_row.append(' ')
-        if month_day % 7 == 0:
-            calender_row.append(month_day)
-        else:
-            calender_row.append(month_day)
+print("mon","tue","wed","thurs","fri","sat","sun")
 
- # def print(calender_row):
-        if len(calender_row) == 7:
-            print(calender_row)
-            calender_row.clear()
-        else:
-            print(calender_row)
-            calender_row.clear()
-
-
-
-for month_day in range(1, number_of_days):
+for month_day in range(1,number_of_days):
     for i in range(day):
-        calender_row.append(' ')
-    if month_day % 7 == 0:
-        calender_row.append(month_day)
+        Calender_row.append(" ")
+
+    if month_day%7==0:
+        sl.add(month_day)
     else:
-        calender_row.append(month_day)
-if len(calender_row) == 7:
-    print(calender_row)
-    calender_row.clear()
-from calendar import Calendar
+        sl.add(month_day)
 
-from DataStructure.Linkedlist_Utility.Linkedlist_Utility import Singlelinkedlist
 
-ll = Singlelinkedlist()
-calender = Calendar()
-year = int(input("Enter year:"))
-month = input("Enter month : ")
-# number_of_days = calender.month_day(month, year)
-print("Days in month are : ")
-for iterating_value in range(1, calender.month_day(month,year)+1):
-    ll.append(iterating_value)
-ll.print()
+sl.display()
+
+
 
 
