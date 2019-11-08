04ba437c946f75ada5d51c6fbdca88b59a7e90c3
sachinlokesh05 <56384957+sachinlokesh05@users.noreply.github.com>
2019-11-08 16:22:26 +0530

    Delete main_Number_Bst.py

--- Binary_Search_Tree/main_Number_Bst.py
+++
@@ -1,9 +0,0 @@
-from Binary_Search_Tree.Number_Bst import Node
-tree=Node(3)
-
-#input list
-list = [5, 3, 4, 5, 6, 8, 6]
-for i in range(len(list)):
-    tree.insertion(list[i])
-#calling the printtree method to print
-tree.PrintTree()
