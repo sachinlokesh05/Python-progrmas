3f0d813f87474d1e53721b20a98edc53c64c3b45
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2019-11-19 23:23:37 +0530

    [sachin] Add.and Done with Design patterns

--- myapp2/myapp4/testapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from testapp.models import Employee
-# Create your views here.
-
-
-def employee_info(request):
-
-    employee = Employee.objects.all()
-    return render(request, 'result.html', {'employee': employee})
