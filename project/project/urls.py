8c5dc20548b878620ede240f27c2687b35a2d425
sachin <56384957+sachinlokesh05@users.noreply.github.com>
2020-01-07 09:54:03 +0530

    [sachin]Add.updated urls.py of project

--- project/project/urls.py
+++ project/project/urls.py
@@ -7,7 +7,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fundoo/', include('fundoo.urls')),
+    path('user/', include('fundoo.urls')),
+    path('notes/', include('note.urls')),
     url('fundoo/', schema_view, name="swagger"),
     path(r'oauth/', include('social_django.urls', namespace='social')),  # <--
     path(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
